9dd2f4b3571aba3e93771b2204f454a19fc570dc
jonasfover <jonasfovergaard@gmail.com>
2019-08-15 18:12:23 +0200

    Updates

---
+++ precache-manifest.f3390027282d8838a69c654a80e665d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e03d88f319cf91e98720c8225a35a3e1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "078944b0b858b8b0642d",
+    "url": "/monsters-rolodex/static/css/main.18195180.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "078944b0b858b8b0642d",
+    "url": "/monsters-rolodex/static/js/main.8441deb8.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
